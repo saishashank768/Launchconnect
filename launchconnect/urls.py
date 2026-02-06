81b175e7157ffc4986acf898180daed16b96c0b6
Mukesh Reddy <mukeshreddystart@gmail.com>
2026-02-06 11:44:27 +0530

    ....

---
+++ launchconnect/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path('students/', include('students.urls')),
+    path('startups/', include('startups.urls')),
+    path('jobs/', include('jobs.urls')),
+    path('applications/', include('applications.urls')),
+    path('founder-collab/', include('founder_collab.urls')),
+    path('admin-panel/', include('admin_panel.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
